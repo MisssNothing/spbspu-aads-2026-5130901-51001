a488da59be6fff4cd46c93ed192210eb527d4da4
petrenko.esmira <esmira3942@gmail.com>
2026-06-22 15:00:34 +0300

    test one file

--- petrenko.esmira/S3/test-graph.cpp
+++ petrenko.esmira/S3/test-graph.cpp
@@ -1,4 +1,3 @@
-#define BOOST_TEST_MODULE GraphTests
 #include <boost/test/included/unit_test.hpp>
 #include "graph.hpp"
 #include <string>
--- petrenko.esmira/S3/test-sha1.cpp
+++ petrenko.esmira/S3/test-sha1.cpp
@@ -1,4 +1,3 @@
-#define BOOST_TEST_MODULE SHA1Tests
 #include <boost/test/included/unit_test.hpp>
 #include "sha1.hpp"
 #include <string>
